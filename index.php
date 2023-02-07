16a4961ebee73075dd2c7da8ca811301474608ca
Alessandro-Villani <alessvill@libero.it>
2023-02-07 15:35:45 +0100

    added style

--- index.php
+++ index.php
@@ -51,8 +51,12 @@ $hotels = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONTAWESOME -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css' integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ==' crossorigin='anonymous' />
     <!-- BOOTSTRAP -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+    <!-- STYLESHEET -->
+    <link rel="stylesheet" href="styles/style.css">
     <!-- TITLE -->
     <title>Hotels</title>
 </head>
@@ -60,17 +64,33 @@ $hotels = [
 <body>
     <div class="container py-5 text-center">
         <h1>Hotel</h1>
-        <hr>
-        <table>
-            <?php foreach ($hotels as $hotel) : ?>
+        <hr class="mb-5">
+        <table class="table">
+            <thead>
                 <tr>
-                    <th><?= $hotel['name'] ?></th>
-                    <th><?= $hotel['description'] ?></th>
-                    <th><?= $hotel['parking'] ?></th>
-                    <th><?= $hotel['vote'] ?></th>
-                    <th><?= $hotel['distance_to_center'] ?></th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
                 </tr>
-            <?php endforeach ?>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <th><?= $hotel['name'] ?></th>
+                        <th><?= $hotel['description'] ?></th>
+                        <th><?= $hotel['parking'] ? '<i class="fa-solid fa-circle-check"></i>' : '<i class="fa-solid fa-circle-xmark"></i>' ?></th>
+                        <th><?php for ($i = 0; $i < $hotel['vote']; $i++) : ?>
+                                <?= '<i class="fa-solid fa-star"></i>' ?>
+                            <?php endfor; ?>
+                            <?php for ($i = 0; $i < 5 - $hotel['vote']; $i++) : ?>
+                                <?= '<i class="fa-regular fa-star"></i>' ?>
+                            <?php endfor; ?>
+                        </th>
+                        <th><?= $hotel['distance_to_center'] ?> Km</th>
+                    </tr>
+                <?php endforeach ?>
         </table>
     </div>
 </body>
